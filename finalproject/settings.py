c95a62f446e29490b0193c336c5f0a69a2390dbe
unknown <elcreidoluis@gmail.com>
2021-06-09 12:37:36 -0400

    Debug set to False

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '13kl@xtukpwe&xj2xoysxe9_6=tf@f8ewxer5n&ifnd46+6$%8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = bool(os.environ.get('DJANGO_DEBUG', True) )
+DEBUG = False
 
 ALLOWED_HOSTS = ['luisgz.herokuapp.com', '127.0.0.1']
 
